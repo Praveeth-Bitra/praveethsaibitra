ac82ceded75880b78cd90945d5f30bb60015f71c
Praveeth Bitra <praveethbitra@gmail.com>
2025-09-13 15:12:20 +0530

    First Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/", // 👈 important for GitHub Pages + custom domain
   server: {
     host: "::",
     port: 8080,
